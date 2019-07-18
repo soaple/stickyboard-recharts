1d6059617c3f745897019fe1f7d98ba6de22d07b
Soaple <inje@soaple.io>
2019-07-18 22:54:50 +0900

    React component packaging option added.

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports = require('./src/ReLineChart').default;
+module.exports = require('./src/DateAxisTick').default;
+module.exports = require('./src/DateOnlyAxisTick').default;
+module.exports = require('./src/SimpleDateAxisTick').default;

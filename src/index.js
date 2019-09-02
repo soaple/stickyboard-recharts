93f35e52dc0a0c6dd91b720fdae294b44bbe8e75
Soaple <inje@soaple.io>
2019-09-02 18:33:50 +0900

    Build setting updated.

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import LineChart from './SBR_LineChart';
+import BarChart from './SBR_BarChart';
+import ComposedChart from './SBR_ComposedChart';
+import PieChart from './SBR_PieChart';
+import RadarChart from './SBR_RadarChart';
+import AreaChart from './SBR_AreaChart';
+import ScatterChart from './SBR_ScatterChart';
+import Treemap from './SBR_Treemap';
+import DateAxisTick from './DateAxisTick';
+import DateOnlyAxisTick from './DateOnlyAxisTick';
+import SimpleDateAxisTick from './SimpleDateAxisTick';
+
+export {
+    LineChart,
+    BarChart,
+    ComposedChart,
+    PieChart,
+    RadarChart,
+    AreaChart,
+    ScatterChart,
+    Treemap,
+    DateAxisTick,
+    DateOnlyAxisTick,
+    SimpleDateAxisTick,
+};
